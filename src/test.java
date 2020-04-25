ba3ac4a14a368baf69755ab23c539e3398a68cca
breakwave <2533706897@qq.com>
2020-04-25 11:28:16 +0800

    test    class

---
+++ src/test.java
@@ -0,0 +1,13 @@
+/**
+ * @PACKAGE_NAME: PACKAGE_NAME
+ * @USER: test
+ * @DATE: 2020/4/25 11:26
+ * @PROJECT_NAME: OKBaby
+ * @Version 1.0
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("你好，世界");
+    }
+}
